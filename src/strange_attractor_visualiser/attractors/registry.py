1fc28b213a5895392e5709cf6e618b5b808cc37c
aymenhafeez <hafeezaymen2357@gmail.com>
2026-04-13 22:18:02 +0100

    add: updated attractor registry

--- src/strange_attractor_visualiser/attractors/registry.py
+++ src/strange_attractor_visualiser/attractors/registry.py
@@ -1,6 +1,8 @@
 from ..attractors.aizawa import aizawa_attractor
+from ..attractors.arneodo import arneodo_attractor
 from ..attractors.burke_shaw import burke_shaw_attractor
 from ..attractors.dadras import dadras_attractor
+from ..attractors.halvorsen import halvorsen_attractor
 from ..attractors.lorenz import lorenz_attractor
 from ..attractors.rossler import rossler_attractor
 from ..attractors.rucklidge import rucklidge_attractor
@@ -14,4 +16,6 @@ ATTRACTORS = {
     "Aizawa": aizawa_attractor,
     "Rucklidge": rucklidge_attractor,
     "Burke-shaw": burke_shaw_attractor,
+    "Arneoda": arneodo_attractor,
+    "Halvorsen": halvorsen_attractor,
 }
