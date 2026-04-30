0b387be2b801d3f433b37e59874efef0bc1b363a
aymenhafeez <hafeezaymen2357@gmail.com>
2026-04-30 15:35:19 +0100

    fix: updated aizawa attractor default param values

--- src/strange_attractor_visualiser/attractors/aizawa.py
+++ src/strange_attractor_visualiser/attractors/aizawa.py
@@ -25,14 +25,14 @@ aizawa_attractor = AttractorConfig(
     "aizawa",
     _aizawa,
     params=[
-        AttractorParam("$a$", 1.0, -0.55, 40.0, 0.01),
+        AttractorParam("$a$", 0.95, -0.55, 40.0, 0.01),
         AttractorParam("$b$", 0.7, -2.0, 25.0, 0.01),
         AttractorParam("$c$", 0.6, -10.0, 10.0, 0.01),
         AttractorParam("$d$", 3.5, 0.0, 500.0, 0.001),
         AttractorParam("$e$", 0.25, 0.0, 60.0, 0.01),
         AttractorParam("$f$", 0.1, -2.10, 20.0, 0.01),
     ],
-    initial_conditions=[-0.29, -0.25, -0.59],
+    initial_conditions=[0.1, 0.0, 0.0],
     time_defaults={"t_min": 0, "t_max": 30, "n": 30000},
     description=(
         "The Aizawa attractor differs in shape from the classic winged shape of the \
