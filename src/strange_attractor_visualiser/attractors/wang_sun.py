346258119d5df197b3c8d726b01092be2944f1be
aymenhafeez <hafeezaymen2357@gmail.com>
2026-06-09 15:33:36 +0100

    fix: wang-sun attractor info

--- src/strange_attractor_visualiser/attractors/wang_sun.py
+++ src/strange_attractor_visualiser/attractors/wang_sun.py
@@ -35,10 +35,10 @@ wang_sun_attractor = AttractorConfig(
     initial_conditions=[0.1, 0.1, 0.1],
     time_defaults={"t_min": 0, "t_max": 750, "n": 30000},
     description=(
-        "The Wang-Sun system is a three-dimensional chaotic system that generates "
-        "complex multi-wing attractors. It is characterized by its ability to "
-        "produce intricate, symmetric loops that resemble a double-scroll, but "
-        "with more aggressive twisting and folding along its trajectories."
+        "The Wang-Sun system is a three-dimensional chaotic system that generates \
+            complex multi-wing attractors. It is characterized by its ability to \
+            produce intricate, symmetric loops that resemble a double scroll, but \
+            with more aggressive twisting and folding along its trajectories."
     ),
     equation_text=r"$\\\dot{x}=ax+cyz,\\\dot{y}=bx+dy-xz,\\\dot{z}=ez+fxy$",
     presets={
@@ -58,13 +58,13 @@ wang_sun_attractor = AttractorConfig(
             "$e$": -4.3853,
             "$f$": -130.9962,
         },
-        "Cone": {
-            "$a$": 0.5418,
-            "$b$": -0.0688,
-            "$c$": 1.8342,
-            "$d$": -0.6933,
-            "$e$": -0.2657,
-            "$f$": -465.7684,
+        "Spread spiral": {
+            "$a$": 0.7582,
+            "$b$": 0.1076,
+            "$c$": 6.6217,
+            "$d$": -0.1187,
+            "$e$": -1.4410,
+            "$f$": -360.8325,
         },
         "Spiral": {
             "$a$": 0.5091,
@@ -76,13 +76,13 @@ wang_sun_attractor = AttractorConfig(
         },
     },
     prompts=[
-        "Adjusting $a$ and $d$ will determine if the system converges to a point or "
-        "explodes into chaos.",
-        "The parameter $c$ influences the strength of the coupling between the wings. "
-        "Higher values tend to stretch the attractor along the x-axis.",
-        "Small changes in $b$ can shift the symmetry of the wings causing one side to "
-        "dominate or the entire structure to tilt.",
-        "$e$ and $f$ control the vertical stability. Modifying $e$ will "
-        "often squash or elongate the attractor along the z-axis core.",
+        "Adjusting $a$ and $d$ will determine if the system converges to a point or \
+            explodes into chaos.",
+        "$c$ influences the strength of the coupling between the wings. Higher values \
+                tend to stretch the attractor along the x-axis.",
+        "Small changes in $b$ can shift the symmetry of the wings causing one side to \
+            dominate or the entire structure to tilt.",
+        "$e$ and $f$ control the vertical stability. Modifying $e$ will \
+            often squash or elongate the attractor along the z-axis.",
     ],
 )
