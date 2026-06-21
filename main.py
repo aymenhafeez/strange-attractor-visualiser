9e804ef408901d2a8b4c771d57caaef8ce227c0d
aymenhafeez <hafeezaymen2357@gmail.com>
2026-06-21 21:09:25 +0100

    fix: point streamlit to dependency info

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent / "src"))
+
 from strange_attractor_visualiser.ui.plot_page import render_plot_page
 
 if __name__ == "__main__":
